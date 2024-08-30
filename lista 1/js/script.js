ce60e206301520552f91951cf6cbb2f8e964a29f
Laviniama <129920113+Laviniama@users.noreply.github.com>
2024-08-30 19:32:57 -0300

    Add files via upload

---
+++ lista 1/js/script.js
@@ -0,0 +1,12 @@
+//alert("Olá Daniel")
+//var resposta = confirm("Deseja excluir");
+//console.log(resposta);
+//var a= 10;
+//var b= 33;
+//alert(a+b);
+
+function somar(a,b){
+
+		return a+ b;}
+
+console.log(somar(2, 2));
